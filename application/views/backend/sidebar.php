23b742cb3e7c037bd21d906bc9bef0c3ca5227e3
Marcelo tms <marcelojoc@gmail.com>
2017-12-01 11:02:22 -0300

    cambio en menu sidebar 1

--- application/views/backend/sidebar.php
+++ application/views/backend/sidebar.php
@@ -143,6 +143,15 @@
 					<b class="arrow"></b>
 						<ul class="submenu">
 
+
+						<li class="">
+								<a href="<?php echo base_url('gener/')?>">
+									<i class="menu-icon fa fa-caret-right"></i>
+									General
+								</a>
+								<b class="arrow"></b>
+							</li>
+
 							<li class="">
 								<a href="<?php echo base_url('gener/est')?>">
 									<i class="menu-icon fa fa-caret-right"></i>
@@ -166,6 +175,7 @@
 								</a>
 								<b class="arrow"></b>
 							</li>
+
 						</ul>
 				</li>				
 				<?php 		}
